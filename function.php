23fa8235c33cadb582b4b0bea364ba7b18ebc482
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-17 23:39:45 +0100

    copiata funzione nel nuovo file function.php

--- function.php
+++ function.php
@@ -1,1 +1,29 @@
 <?php
+
+
+function passwordGenerator($passwordLenght, $lettMai, $lettMin)
+{
+    $array = array_merge($lettMai, $lettMin);
+    $password = '';
+    if ($passwordLenght > 1) {
+        $arrayKey = array_rand($array, $passwordLenght);
+        shuffle($arrayKey);
+    } elseif ($passwordLenght == 1) {
+        $arrayKey = array_rand($array, $passwordLenght);
+    } else {
+        $arrayKey = 0;
+    }
+
+    if ($passwordLenght > 1) {
+
+        foreach ($arrayKey as $value) {
+            $password .= $array[$value];
+        };
+    } elseif ($passwordLenght == 1) {
+        $password .= $array[$arrayKey];
+    } elseif (intval($passwordLenght) == 0) {
+        $password .= 'Ti consiglio di riconsiderare la lunghezza della password. Una password sicura contiene ALMENO un carattere.';
+    }
+
+    return $password;
+}
