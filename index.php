0d050e31037d711c85dce36bc6c4ad55a8cc7041
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-17 23:46:45 +0100

    eliminato funzione commentata in foglio index.php

--- index.php
+++ index.php
@@ -7,34 +7,6 @@ $alfabetoMaiuscolo = range('A', 'Z');
 $numeri = range(0, 9);
 $simboli = array('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', ';', ':', ',');
 
-/* funzione per generazione password */
-/* function passwordGenerator($passwordLenght, $lettMai, $lettMin)
-{
-    $array = array_merge($lettMai, $lettMin);
-    $password = '';
-    if ($passwordLenght > 1) {
-        $arrayKey = array_rand($array, $passwordLenght);
-        shuffle($arrayKey);
-    } elseif ($passwordLenght == 1) {
-        $arrayKey = array_rand($array, $passwordLenght);
-    } else {
-        $arrayKey = 0;
-    }
-
-    if ($passwordLenght > 1) {
-
-        foreach ($arrayKey as $value) {
-            $password .= $array[$value];
-        };
-    } elseif ($passwordLenght == 1) {
-        $password .= $array[$arrayKey];
-    } elseif (intval($passwordLenght) == 0) {
-        $password .= 'Ti consiglio di riconsiderare la lunghezza della password. Una password sicura contiene ALMENO un carattere.';
-    }
-
-    return $password;
-} */
-
 ?>
 
 
