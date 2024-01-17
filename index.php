f72218abcd29365aed9aabc5b7d5c1b4f61d32ff
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-17 23:14:44 +0100

    corretto funzione per generare password

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <?php
 
-$passwordLenght = $_GET['passwordLenght'];
+$passwordLenght = isset($_GET['passwordLenght']) ? $_GET['passwordLenght'] : 0;
 $alfabetoMinuscolo = range('a', 'z');
+$alfabetoMaiuscolo = range('A', 'Z');
+$numeri = range(0, 9);
+$simboli = array('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', ';', ':', ',');
 
 /* funzione per generazione password */
-function passwordGenerator($passwordLenght, $alfabetoMinuscolo)
+function passwordGenerator($passwordLenght, $lettMai, $lettMin)
 {
-    $alfabetoMinuscolo = range('a', 'z');
-    $alfabetoMaiuscolo = range('A', 'Z');
-    $numeri = range(0, 9);
-    $simboli = array('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', ';', ':', ',');
-
-
-    $arrayKey = array_rand($alfabetoMinuscolo, $passwordLenght);
-
+    $array = array_merge($lettMai, $lettMin);
     $password = '';
-    foreach ($arrayKey as $value) {
-        $password .= $alfabetoMinuscolo[$value];
-    };
+    if ($passwordLenght > 0) {
+        $arrayKey = array_rand($array, $passwordLenght);
+    } else {
+        $arrayKey = 0;
+    }
 
+    if ($passwordLenght > 1) {
+
+        foreach ($arrayKey as $value) {
+            $password .= $array[$value];
+        };
+    } elseif ($passwordLenght == 1) {
+        $password .= $array[$arrayKey];
+    } elseif (intval($passwordLenght) == 0) {
+        $password .= 'inserire lunghezza password';
+    }
 
     return $password;
 }
@@ -35,7 +43,8 @@ function passwordGenerator($passwordLenght, $alfabetoMinuscolo)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <!-- boostrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
@@ -43,7 +52,8 @@ function passwordGenerator($passwordLenght, $alfabetoMinuscolo)
         <form action="./index.php" method="get">
             <div class="mb-3">
                 <label for="passwordLenght" class="form-label">Inserire lunghezza password</label>
-                <input type="number" class="form-control w-25" id="passwordLenght" name="passwordLenght" aria-describedby="passwordLenghtHelp">
+                <input type="number" class="form-control w-25" id="passwordLenght" name="passwordLenght"
+                    aria-describedby="passwordLenghtHelp">
                 <div id="passwordLenghtHelp" class="form-text">Verrà generata una password del numero di caratteri
                     inseriti
                 </div>
@@ -51,7 +61,8 @@ function passwordGenerator($passwordLenght, $alfabetoMinuscolo)
             <button type="submit" class="btn btn-primary">Genera</button>
         </form>
         <div class="prova"><?php echo $passwordLenght ?></div>
-        <div class="prova2">password: <?php echo passwordGenerator($passwordLenght, $alfabetoMinuscolo) ?> </div>
+        <div class="prova2">password:
+            <?php echo passwordGenerator($passwordLenght, $alfabetoMaiuscolo, $alfabetoMinuscolo) ?> </div>
     </main>
 </body>
 
